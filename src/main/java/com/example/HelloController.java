9580c2a5e59ace0f5ad176848347e2c824032951
Marius <m.gzegozevskis@gmail.com>
2017-02-15 13:32:30 +0200

    r

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Marius on 2/15/2017.
+ */
+@RestController
+public class HelloController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
--- src/main/java/org/wildfly/swarm/examples/springdata/EmployeeNotFound.java
+++
@@ -1,7 +0,0 @@
-package org.wildfly.swarm.examples.springdata;
-
-/**
- * @author Ken Finnigan
- */
-public class EmployeeNotFound extends Exception {
-}
