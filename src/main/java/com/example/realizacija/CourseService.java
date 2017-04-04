3ef719e02b54988e1927af79e738d3e963d9c00e
Marius Gžegoževskis <m.gzegozevskis@gmail.com>
2017-04-04 17:19:21 +0300

    hhh

--- src/main/java/com/example/realizacija/CourseService.java
+++
@@ -1,9 +0,0 @@
-package com.example.realizacija;
-
-/**
- * Created by Marius on 2/15/2017.
- */
-public interface CourseService {
-    Iterable<Course> displayAllCourses();
-    Course getCourseById(Integer id);
-}
