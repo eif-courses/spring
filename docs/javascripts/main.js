5360c7861cd7b97328c0cccaba196ed7aba90540
Marius Gžegoževskis <m.gzegozevskis@gmail.com>
2017-04-27 17:28:44 +0300

    update v.1.2

--- docs/javascripts/main.js
+++ docs/javascripts/main.js
@@ -1,9 +1,36 @@
-console.log('This would be the main JS file.');
-var acc = document.getElementsByClassName("accordion");
-var i;
+function randomLinks(){
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-  }
+    var sidebarLinks = document.querySelectorAll("aside a.button");
+    for(d = 0; d < sidebarLinks.length; d++)
+    {
+        var color = "#";
+        for (k = 0; k < 3; k++) {
+            color += ("0" + (Math.random()*230|0).toString(16)).substr(-2);
+        }
+        sidebarLinks[d].style.backgroundColor = color;
+    }
+}
+$(document).ready(function(){
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/lt_LT/sdk.js#xfbml=1&version=v2.8&appId=336920023368615";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    $('ul.tabs').tabs();
+    randomLinks();
+
+    document.getElementById("index").onclick = function() {
+        window.location.href = "index.html";
+    };
+    document.getElementById("security").onclick = function() {
+        window.location.href = "security.html";
+    };
+    document.getElementById("praktika").onclick = function() {
+        window.location.href = "praktika.html";
+    };
+
+});
